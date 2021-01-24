feaf90ea9769e3a081ceca872b068f875e42f958
albert18 <caps18@hotmail.com>
2021-01-24 10:44:02 -0500

    passing event and data binding combined

--- Main_Project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ Main_Project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
 
@@ -9,10 +9,15 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected() {
+    this.recipeSelected.emit();
   }
 
 }
