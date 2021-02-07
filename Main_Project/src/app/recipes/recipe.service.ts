9949947d5d09283f875bb7ca6bf7c8a633f231a1
albert18 <caps18@hotmail.com>
2021-02-07 09:58:50 -0500

    fill recipe service with data

--- Main_Project/src/app/recipes/recipe.service.ts
+++ Main_Project/src/app/recipes/recipe.service.ts
@@ -1,3 +1,12 @@
-export class RecipeService {
+import { Recipe } from './recipe.model';
 
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe - 1', 'This is simply a test - 1', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('A Test Recipe - 2', 'This is simply a test - 2 ', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
