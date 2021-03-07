74cded95361b30647d424934fa51467aca93458a
albert18 <erertech@gmail.com>
2021-03-07 14:37:04 -0500

    angular routes

--- Main_Project/src/app/app.module.ts
+++ Main_Project/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -14,13 +13,6 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-// import { AppRoutingModule } from './app-routing.module';
-
-const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes' },
-  { path: 'recipes', component: RecipesComponent },
-  { path: 'shopping-list', component: ShoppingListComponent }
-];
 
 @NgModule({
   declarations: [
@@ -36,8 +28,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes)
+    FormsModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
